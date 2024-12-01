bca3c8c08acda8d9c699a6b7508b52b9c66fbc45
Darren <darren.hicks@gmail.com>
2024-12-01 01:49:14 -0700

    Adding Sierpinski generator and cryptoquote generator

---
+++ public/js/dbh-dev-console.js
@@ -0,0 +1,30 @@
+function writeDBHToConsole() {
+    console.log(`
+ Thanks for visiting darrenhicks.dev
+
+ DDDDDDDD       BBBBBBBB     HH     HH       DDDDDDDD     EEEEEEEEE  VV         VV
+ DD     DD      BB     BB    HH     HH       DD     DD    EE          VV       VV
+ DD      DD     BB     BB    HH     HH       DD      DD   EE           V       V
+ DD       DD    BBBBBBBB     HHHHHHHHH       DD       DD  EEEEEEE       V     V
+ DD       DD    BB     BB    HH     HH       DD       DD  EE             V   V
+ DD      DD     BB     BB    HH     HH       DD      DD   EE              V V
+ DD     DD      BB     BB    HH     HH       DD     DD    EE               V
+ DDDDDDDD       BBBBBBBB     HH     HH       DDDDDDDD     EEEEEEEEE        V
+`);
+}
+
+// Auto-execute when included in a webpage
+writeDBHToConsole();
+
+function writeAardvarkToConsole() {
+    console.log(`
+       _.---._    /\\\\
+    ./'       "--\`\\//
+  ./              o \\          .-----.
+ /./\\  )______   \\__ \\        ( help! )
+./  / /\\ \\   | \\ \\  \\ \\       /\`-----'
+   / /  \\ \\  | |\\ \\  \\7--- ooo ooo ooo ooo ooo ooo`);
+}
+
+// Auto-execute when included in a webpage
+writeAardvarkToConsole();
